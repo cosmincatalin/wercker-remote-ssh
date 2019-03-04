c2334e5a2c919dac985d135a9c4ac43da4e4e7a8
Cosmin Catalin Sanda <cosmincatalin@gmail.com>
2019-03-04 16:06:23 +0100

    .

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 SAVEIFS=$IFS
 # Change IFS to new line. 
 TST="$WERCKER_REMOTE_SSH_COMMANDS"
-TST=$(echo "$TST" | sed -r 's/\\n/$WERCKER_REMOTE_IFS/g')
+TST=$(echo "$TST" | sed -r 's/\\n/'$WERCKER_REMOTE_IFS'/g')
 IFS=$WERCKER_REMOTE_IFS
 read -r -a commands <<< "$TST"
 # Restore IFS
